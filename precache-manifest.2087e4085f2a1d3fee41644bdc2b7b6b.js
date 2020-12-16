d3c14ee700a2a33679483a5ba031108e901d1998
unknown <yossi.povalaev@gmail.com>
2020-12-16 04:37:46 +0200

    Updates

---
+++ precache-manifest.2087e4085f2a1d3fee41644bdc2b7b6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e77e67bd85f1822fa7a72798683fe0",
+    "url": "/Mern-stack/index.html"
+  },
+  {
+    "revision": "3b1c310a2639c5118975",
+    "url": "/Mern-stack/static/css/2.0da02d22.chunk.css"
+  },
+  {
+    "revision": "3b1c310a2639c5118975",
+    "url": "/Mern-stack/static/js/2.b5d4fe44.chunk.js"
+  },
+  {
+    "revision": "735ba95356c741222224",
+    "url": "/Mern-stack/static/js/main.090fdc36.chunk.js"
+  },
+  {
+    "revision": "9b855e7bf48ded83898a",
+    "url": "/Mern-stack/static/js/runtime~main.e8471ea0.js"
+  }
+]);
